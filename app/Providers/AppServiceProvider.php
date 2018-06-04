d0b6757529a11d27d9026c25cbea848ff41569b5
Jimmyelvis <jinjoe@zoho.com>
2018-06-04 18:22:50 -0500

    updated app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
+use config\database;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,13 @@ class AppServiceProvider extends ServiceProvider
      */
      public function boot(UrlGenerator $url)
       {
-          if(env('APP_ENV') !== 'local')
+          // if(env('APP_ENV') !== 'local')
+          // {
+          //     $url->forceSchema('https');
+          //     echo 'yes';
+          // }
+
+          if('default '== 'mysql-production')
           {
               $url->forceSchema('https');
               echo 'yes';
