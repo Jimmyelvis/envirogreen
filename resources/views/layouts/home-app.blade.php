d8882657ce1eaf11779e21eb7a7c7dc7cea01f87
Jimmyelvis <jinjoe@zoho.com>
2018-06-03 20:30:25 -0500

    update header

--- resources/views/layouts/home-app.blade.php
+++ resources/views/layouts/home-app.blade.php
@@ -17,7 +17,7 @@
 
     <link href="css/styles.css" rel="stylesheet">
 
-    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 
     <!-- Latest compiled and minified JavaScript -->
